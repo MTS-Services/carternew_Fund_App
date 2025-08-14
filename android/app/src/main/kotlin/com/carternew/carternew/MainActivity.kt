7652c314262809ae3e2733de518081f5d303c543
arafat593 <arafathossain593@gmail.com>
2025-08-14 15:48:09 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/carternew/carternew/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.carternew.carternew
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,43 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const ONBODINGS1 = _Paths.ONBODINGS1;
+  static const ONBODINGS2 = _Paths.ONBODINGS2;
+  static const ONBODINGS3 = _Paths.ONBODINGS3;
+  static const LOG_IN = _Paths.LOG_IN;
+  static const PERSONAL_INFO = _Paths.PERSONAL_INFO;
+  static const YOUR_ADDRESS = _Paths.YOUR_ADDRESS;
+  static const TERMS_AND_CONDITIONS = _Paths.TERMS_AND_CONDITIONS;
+  static const GMAIL_CONNECT = _Paths.GMAIL_CONNECT;
+  static const SCANNING_SECTION = _Paths.SCANNING_SECTION;
+  static const PRICING = _Paths.PRICING;
+  static const RESULT = _Paths.RESULT;
+  static const CATEGORIES_LIST = _Paths.CATEGORIES_LIST;
+  static const TRACKER_2 = _Paths.TRACKER_2;
+  static const TRACKER_SINGLE_CLAIM_DETAILS =
+      _Paths.TRACKER_SINGLE_CLAIM_DETAILS;
+  static const PROFILE = _Paths.PROFILE;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const ONBODINGS1 = '/onbodings1';
+  static const ONBODINGS2 = '/onbodings2';
+  static const ONBODINGS3 = '/onbodings3';
+  static const LOG_IN = '/log-in';
+  static const PERSONAL_INFO = '/personal-info';
+  static const YOUR_ADDRESS = '/your-address';
+  static const TERMS_AND_CONDITIONS = '/terms-and-conditions';
+  static const GMAIL_CONNECT = '/gmail-connect';
+  static const SCANNING_SECTION = '/scanning-section';
+  static const PRICING = '/pricing';
+  static const RESULT = '/result';
+  static const CATEGORIES_LIST = '/categories-list';
+  static const TRACKER_2 = '/tracker-2';
+  static const TRACKER_SINGLE_CLAIM_DETAILS = '/tracker-single-claim-details';
+  static const PROFILE = '/profile';
+}
